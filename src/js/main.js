45c66683308e94090fbab3a8647cea2101f51373
Alexey <nikiforov197@gmail.com>
2021-10-21 22:29:50 +0300

    create: add modal window on click to button

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,51 @@
-const hotelSlider = new Swiper('.hotel-slider', {
-    // Optional parameters
-    loop: true,
+$(document).ready(function () {
+    const hotelSlider = new Swiper('.hotel-slider', {
+        // Optional parameters
+        loop: true,
+    
+        // Navigation arrows
+        navigation: {
+          nextEl: '.hotel-slider__button--next',
+          prevEl: '.hotel-slider__button--prev',
+        },
+        effect: "slide",
+    });
+    
+    const reviewsSlider = new Swiper('.reviews-slider', {
+        // Optional parameters
+        loop: true,
+    
+        // Navigation arrows
+        navigation: {
+          nextEl: '.reviews-slider__button--next',
+          prevEl: '.reviews-slider__button--prev',
+        },
+        effect: "slide",
+    });
+    
+    let menuButton = $(".menu-button");
+    menuButton.on('click', function(){
+      console.log("Клик по кнопке меню");
+      $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+    });
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.hotel-slider__button--next',
-      prevEl: '.hotel-slider__button--prev',
-    },
-    effect: "slide",
-});
+    let modalButton = $("[data-toggle=modal]");
+    let closeModalButton = $(".modal__close");
+    modalButton.on('click', openModal)
+    closeModalButton.on("click", closeModal);
 
-const reviewsSlider = new Swiper('.reviews-slider', {
-    // Optional parameters
-    loop: true,
+    function openModal(){
+      let modalOverlay = $(".modal__overlay");
+      let modalDialog = $(".modal__dialog");
+      modalOverlay.addClass("modal__overlay--visible");
+      modalDialog.addClass("modal__dialog--visible");
+    }
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.reviews-slider__button--next',
-      prevEl: '.reviews-slider__button--prev',
-    },
-    effect: "slide",
-});
-
-let menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener('click', function(){
-  console.log("Клик по кнопке меню");
-  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+      function closeModal(event) {
+        event.preventDefault();
+        let modalOverlay = $(".modal__overlay");
+        let modalDialog = $(".modal__dialog");
+        modalOverlay.removeClass("modal__overlay--visible");
+        modalDialog.removeClass("modal__dialog--visible");
+      }
 });
